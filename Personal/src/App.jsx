e25ff3a62ce6bb0eb46bad38178c2b64cfff42c6
Angus Sun <angussun.mail@gmail.com>
2025-09-18 22:01:58 -0400

    fix: projects page works with vercel

--- Personal/src/App.jsx
+++ Personal/src/App.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Projects } from "./pages/Projects";
 
 function App() {
   return (
-    <> 
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />}/>
-          <Route path = "projects" element = { <Projects />} />
-          <Route path = "*" element = { <NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Routes>
+      <Route index element={<Home />}/>
+      <Route path="projects" element={<Projects />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
