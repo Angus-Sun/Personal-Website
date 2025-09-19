80ac221baad06876e85d255c730352cce76337c3
Angus Sun <angussun.mail@gmail.com>
2025-09-18 21:41:01 -0400

    fix: fixed main.jsx

--- Personal/src/main.jsx
+++ Personal/src/main.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
+  </StrictMode>,
+)
