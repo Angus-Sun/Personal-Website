476065fc857d980bf6c69c9a89f0611c715271cd
Angus Sun <angussun.mail@gmail.com>
2025-11-12 18:10:50 -0500

    fix: updated UI

--- Personal/src/components/Footer.jsx
+++ Personal/src/components/Footer.jsx
@@ -20,12 +20,6 @@ const socialLinks = [
     colorClass: "hover:text-primary-foreground",
   },
   {
-    name: "Resume",
-    icon: Newspaper,
-    href: "/Angus_Sun_Resume.pdf",
-    colorClass: "hover:text-primary-foreground",
-  },
-  {
     name: "Twitter",
     icon: Twitter,
     href: "https://www.twitter.com/_angussun_",
